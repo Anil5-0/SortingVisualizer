2a05e0d8a3e34b6852038328fbed9bf4af622c4c
Anil <anilkumar88975@gmail.com>
2020-07-03 21:00:26 +0530

    build

---
+++ build/precache-manifest.e17875516858f94aa6ba4bd8b5f48970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9611d7b04dafe349545faab8632b0a93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fd85c99fb9c222ca483",
+    "url": "/static/css/main.05792154.chunk.css"
+  },
+  {
+    "revision": "8110a716583556f666e1",
+    "url": "/static/js/2.d02140b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d02140b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd85c99fb9c222ca483",
+    "url": "/static/js/main.ecafb292.chunk.js"
+  },
+  {
+    "revision": "cd4c12c61560536dd7c3",
+    "url": "/static/js/runtime-main.ae951214.js"
+  }
+]);
